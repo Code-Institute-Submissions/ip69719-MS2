9974f4f71ab21f13aa90f14eb4bc18c86c867c8a
ip69719 <ipeters@mail.com>
2021-10-15 00:55:45 +0000

    Create questions array for the quiz#

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Create questions for the quiz
+let questions = [{
+    imgSrc : "assets/images/lexus.png",
+    option1 : "Lexus",
+    option2 : "Lamborghini",
+    option3 : "Lotus",
+    correct : "optionA"
+},{
+    imgSrc : "assets/images/mercedes.png",
+    option1 : "Mazda",
+    option2 : "Mercedes",
+    option3 : "McLaren",
+    correct : "optionB"
+},{
+    imgSrc : "assets/images/toyota.png",
+    option1 : "Tesla",
+    option2 : "Tata",
+    option3 : "Toyota",
+    correct : "optionC"
+}];
+
